f6c42a64075812780c02c738f2a285e2b1d5729e
JADPA-25 <JADPA-25@JADPA-25>
2022-08-04 10:56:03 -0600

    Agregar archivos de proyecto.

---
+++ practica020822/Tarea/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("**Calcular importe y descuento**");
+
+Console.WriteLine(" ");
+double cantidad, precio;
+
+Console.WriteLine("Digite la cantidad de productos que lleva");
+cantidad = double.Parse(Console.ReadLine());
+Console.WriteLine("Digite el precio de los productos");
+precio = double.Parse(Console.ReadLine());
+
+double importe = cantidad * precio;
+double descuento = importe * 0.11;
+double total = importe - descuento;
+
+Console.WriteLine("El importe por los productos es: " + importe.ToString("0.00"));
+Console.WriteLine("EL decuento por los productos es: " + descuento.ToString("0.00"));
+Console.WriteLine("El total a ppagar es de: " + total.ToString("0.00"));
